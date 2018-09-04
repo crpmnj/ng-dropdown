ebbf475a364b39da1e1c4052958898bdd28db7d8
Artem Shulga <temashu@ya.ru>
2018-09-04 18:19:01 +0300

    fix index file

--- index.ts
+++ index.ts
@@ -1,1 +1,13 @@
-export * from './src/app/index';
+export { ClickOutsideDirective } from './src/app/dropdown-module/directives/click-outside.directive';
+export { TemplateNameDirective } from './src/app/dropdown-module/directives/template-name.directive';
+
+export { DropdownComponent } from './src/app/dropdown-module/components/dropdown/dropdown.component';
+
+export { DropdownItem } from './src/app/dropdown-module/data-types/dropdown-item.interface';
+
+export { IterableMapPipe } from './src/app/dropdown-module/pipes/iterable-map.pipe';
+export { LimitPipe } from './src/app/dropdown-module/pipes/limit.pipe';
+export { FilterMapValuePipe } from './src/app/dropdown-module/pipes/filter-map-value.pipe';
+
+export { DropdownConfig } from './src/app/dropdown-module/config/config.interface';
+export { DefaultConfig } from './src/app/dropdown-module/config/default.config';
